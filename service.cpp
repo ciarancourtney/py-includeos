97cea7299feabba724b85683b083b10ee3031f14
ciarancourtney <ciarancour@gmail.com>
2017-08-05 22:55:21 +0100

    Embed very high level python execution into service.start()

--- service.cpp
+++ service.cpp
@@ -1,8 +1,16 @@
 #include <service>
 #include <cstdio>
+#include <Python.h>
+
 
 void Service::start(const std::string& args)
 {
   printf("Hello world - OS included!\n");
   printf("Args = %s\n", args.c_str());
+
+
+  // https://docs.python.org/3.6/extending/embedding.html#very-high-level-embedding
+  Py_Initialize();
+  PyRun_SimpleString("from time import time,ctime\n"
+                       "print('Today is', ctime(time()))\n");
 }
